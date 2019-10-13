4728a6500c7e0b4fa6db33a92e0bb60530b02d16
anastasiiafilatova <anastasiafil3112@gmail.com>
2019-10-13 12:54:21 +0300

    add demo face recognition

--- src/face_recognition_ui.py
+++ src/face_recognition_ui.py
@@ -7,6 +7,7 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5.QtCore import pyqtSignal
 from HeadPoseEstimation.head_pose_estimation import HeadPoseEstimator
+import faceRecognition
 
 class ShowVideo(QtCore.QObject):
     camera_port = 0
@@ -15,8 +16,20 @@ class ShowVideo(QtCore.QObject):
     run_video = False
     mode = 0
     estimator = None
+    known_face_encoding = []
+    known_face_names = []
+
     def __init__(self, parent = None):
         super(ShowVideo, self).__init__(parent)
+
+    def add_person(self, img_path, name):
+        img = faceRecognition.load_image_file(img_path)
+        faceboxes, landmarks = self.estimator.face_detector.get_faceboxes(img)
+        facebox = faceboxes[0] if len(faceboxes) >= 1 else None
+        l = landmarks[0]
+        upd_facebox = [[int(facebox[1]), int(facebox[2]), int(facebox[3]), int(facebox[0])]]
+        my_face_encoding = faceRecognition.face_encodings(img, l, upd_facebox)[0]
+        return my_face_encoding, name
 
     @QtCore.pyqtSlot()
     def startVideo(self):
@@ -25,6 +38,11 @@ class ShowVideo(QtCore.QObject):
 
         if self.estimator is None:
             self.estimator = HeadPoseEstimator(sample_image)
+
+        face_encoding_templ, name_templ = self.add_person("/home/anastasiia/Documents/DeepFR/Nastya_train_12.jpg",
+                                                          "NASTYA")
+        self.known_face_encoding.append(face_encoding_templ)
+        self.known_face_names.append(name_templ)
 
         while self.run_video:
             ret, image = self.camera.read()
@@ -53,15 +71,31 @@ class ShowVideo(QtCore.QObject):
         return image
 
     def face_recog(self, image):
-        faceboxes = self.estimator.face_detector.get_faceboxes(image)
+        faceboxes, landmarks = self.estimator.face_detector.get_faceboxes(image)
         facebox = faceboxes[0] if len(faceboxes) >= 1 else None
+        l = landmarks[0]
+        upd_facebox = [[int(facebox[1]), int(facebox[2]), int(facebox[3]), int(facebox[0])]]
+        face_encodings = faceRecognition.face_encodings(image, l, upd_facebox)
         if facebox is not None:
-            #print(facebox)
+
+            matches = faceRecognition.compare_faces(self.known_face_encoding, face_encodings[0])
+            name = "Unknown"
+
+            '''
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = self.known_face_names[first_match_index]
+            '''
+
+            face_distances = faceRecognition.face_distance(self.known_face_encoding, face_encodings[0])
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = self.known_face_names[best_match_index]
             cv2.rectangle(image,
-                        (int(facebox[0]), int(facebox[1])),
-                        (int(facebox[2]), int(facebox[3])), (0, 255, 0))
+                          (int(facebox[0]), int(facebox[1])),
+                          (int(facebox[2]), int(facebox[3])), (0, 255, 0))
             dist = math.fabs(int(facebox[0]) - int(facebox[2])) / 3
-            cv2.putText(image, "Unknown", (int(facebox[0] + dist), int(facebox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+            cv2.putText(image, name, (int(facebox[0] + dist), int(facebox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                         (255, 0, 0),
                         1)
         return image
